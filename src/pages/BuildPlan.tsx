fb1e365e8edd6242127fed38a57da50c372d876a
smitiwis <sistemas.luismi@gmail.com>
2023-08-18 23:44:01 -0500

    instalacion de accordion desk

--- src/pages/BuildPlan.tsx
+++ src/pages/BuildPlan.tsx
@@ -1,4 +1,14 @@
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+
+// Demo styles, see 'Styles' section below for some notes on use.
+import "react-accessible-accordion/dist/fancy-example.css";
 
 import { Container } from "../components/Layouts/Container";
 import { Stepper } from "../components/Stepper/Stepper";
@@ -79,7 +89,37 @@ export const BuildPlanPage = () => {
             </TabList>
 
             <TabPanel>
-              <h2>ASD SS 1</h2>
+              <Accordion>
+                <AccordionItem>
+                  <AccordionItemHeading>
+                    <AccordionItemButton>
+                      What harsh truths do you prefer to ignore?
+                    </AccordionItemButton>
+                  </AccordionItemHeading>
+                  <AccordionItemPanel>
+                    <p>
+                      Exercitation in fugiat est ut ad ea cupidatat ut in
+                      cupidatat occaecat ut occaecat consequat est minim minim
+                      esse tempor laborum consequat esse adipisicing eu
+                      reprehenderit enim.
+                    </p>
+                  </AccordionItemPanel>
+                </AccordionItem>
+                <AccordionItem>
+                  <AccordionItemHeading>
+                    <AccordionItemButton>
+                      Is free will real or just an illusion?
+                    </AccordionItemButton>
+                  </AccordionItemHeading>
+                  <AccordionItemPanel>
+                    <p>
+                      In ad velit in ex nostrud dolore cupidatat consectetur ea
+                      in ut nostrud velit in irure cillum tempor laboris sed
+                      adipisicing eu esse duis nulla non.
+                    </p>
+                  </AccordionItemPanel>
+                </AccordionItem>
+              </Accordion>
             </TabPanel>
             <TabPanel>
               <h2>AA SS 2</h2>
