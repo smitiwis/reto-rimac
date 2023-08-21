fae661d04a9d46d03c35af8b6e2e8a70888c437e
smitiwis <sistemas.luismi@gmail.com>
2023-08-21 15:55:27 -0500

    Llevar a desktop el header

--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -1,14 +1,30 @@
-import { ItemNavbar } from "./ItemNavbar";
-import { itemsNavBar } from "../../constants";
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
-
   return (
     <nav className="nav-main">
       <ul className="nav-list">
-        {itemsNavBar.map((items) => (
-          <ItemNavbar {...items} key={items.image}/>
-        ))}
+        <li className="nav-list__item">
+          <Link className="nav-list__link" to={"/inicio"}>
+            <img
+              className="nav-list__image mr-1"
+              src="/images/logo-rimac.jpg"
+              alt="logo rimac"
+            />
+          </Link>
+        </li>
+        <li className="nav-list__item">
+          <Link className="nav-list__link" to={"/inicio"}>
+            <span className="d-none d-md-block mr-3 rimac-checkbox__label">¿Tienes alguna duda?</span>
+            <img
+              className="nav-list__image mr-1"
+              src="/images/icon-phone.jpg"
+              alt="icon-phone"
+            />
+            <span className="text-call d-block d-md-none">Llámanos</span>
+            <span className="text-call d-none d-md-block">(01) 411 6001</span>
+          </Link>
+        </li>
       </ul>
     </nav>
   );
